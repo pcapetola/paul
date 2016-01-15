338b85cac80205dd1c0bc93a1cc64730e2427ac7
Uzair Rahim <uzair.rahim@redbookconnect.com>
2016-01-14 20:15:37 -0600

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+//  Navigation Change on Scroll *********************************
+// --------------------------------------------------------------
+$(window).scroll(function() {
+    var windowScroll = $(window).scrollTop();
+    if (windowScroll >= 648) {
+        $('nav, section.hero').addClass('stickyNav');
+    } else {
+        $('nav, section.hero').removeClass('stickyNav');
+    }
+});
+
+//  Auto-updating Copyright Year ********************************
+// --------------------------------------------------------------
+$(document).ready(function() {
+	$('footer .varContent').text(new Date().getFullYear()); 
+});
+
+
+// Resume Header Content Switcher - (Meter Elements) ************
+// --------------------------------------------------------------
+$(document).ready(function() {
+	$('.contentSwitcher .meter').mouseover(function() {
+		if( !$(this).hasClass('active') ){
+			$(".meter").removeClass('active');
+			$(this).addClass('active');
+        };
+    });
+});
